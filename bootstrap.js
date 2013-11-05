83cc24bc40115fcccbea1b8052fde05ba941aedd
Nathan Osman <admin@quickmediasolutions.com>
2013-11-04 22:05:37 -0800

    Bumped up minimum supported version to FF10 and added bootstrap.js stub.

---
+++ bootstrap.js
@@ -0,0 +1,16 @@
+function startup(data, reason) {
+    //...
+}
+
+function shutdown(data, reason) {
+    //...
+}
+
+function install(data, reason) {
+    // Do nothing
+}
+
+function uninstall(data, reason) {
+    // Do nothing
+}
+
