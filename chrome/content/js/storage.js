98efad7270455a27119166ec0df53b6c1e805889
Nathan Osman <admin@quickmediasolutions.com>
2013-11-11 12:29:12 -0800

    Began adding storage provider.

---
+++ chrome/content/js/storage.js
@@ -0,0 +1,14 @@
+/*
+ * storage.js - Provides storage for requests and parameters
+ * Copyright 2013 - Nathan Osman
+ */
+
+function Storage() {
+    
+    // Load the services and fileutils modules
+    Components.utils.import("resource://gre/modules/Services.jsm");
+    Components.utils.import("resource://gre/modules/FileUtils.jsm");
+    
+    // Load the file containing the SQLite database
+    //...
+}
