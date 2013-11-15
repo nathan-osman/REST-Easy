7db5daa253a70b4a76476271445ad8616ebdb511
Nathan Osman <admin@quickmediasolutions.com>
2013-11-15 11:30:59 -0800

    Moved the menu item to the Web Developer menu in both the appmenu and Tools menu.

--- bootstrap.js
+++ bootstrap.js
@@ -5,29 +5,39 @@ function startup(data, reason) {
     // Load the watchWindows function
     Components.utils.import('chrome://resteasy/content/js/modules/watchwindows.jsm');
     
-    // Register our item in the tools menu
+    // Register our item in the web developer menus
     watchWindows(function(window) {
         
-        // Create the menu item
-        var menuItem = window.document.createElementNS(NS_XUL, 'menuitem');
-        menuItem.setAttribute('id',        'resteasy');
-        menuItem.setAttribute('label',     'REST Easy');
-        menuItem.setAttribute('accesskey', 'R');
-        menuItem.addEventListener('command', function() {
+        // Creates and returns the menu item with the provided prefix in the provided menu
+        function createMenuItem(menu, prefix) {
             
-            // Open the main.html page in a new tab
-            var tab = window.document.getElementById('content').addTab('chrome://resteasy/content/main.html');
-            tab.parentElement.selectedItem = tab;
+            var menuItem = window.document.createElementNS(NS_XUL, 'menuitem');
+            menuItem.setAttribute('id',        prefix + '_resteasy');
+            menuItem.setAttribute('label',     'REST Easy');
+            menuItem.setAttribute('accesskey', 'R');
+            menuItem.addEventListener('command', function() {
+                
+                // Open the main.html page in a new tab
+                var tab = window.document.getElementById('content').addTab('chrome://resteasy/content/main.html');
+                tab.parentElement.selectedItem = tab;
+                
+            }, true);
             
-        }, true);
+            // First append a separator, then the menu item
+            window.document.getElementById(menu).appendChild(window.document.createElementNS(NS_XUL, 'menuseparator'));
+            window.document.getElementById(menu).appendChild(menuItem);
+            return menuItem;
+        }
         
         // Add it to the toolbar
-        window.document.getElementById('menu_ToolsPopup').appendChild(menuItem);
+        var appmenu_resteasy = createMenuItem('appmenu_webDeveloper_popup', 'appmenu');
+        var menu_resteasy    = createMenuItem('menuWebDeveloperPopup',      'menu');
         
         // Remember to remove the menu item when the addon is unloaded
         unload(function() {
             
-            window.document.getElementById('menu_ToolsPopup').removeChild(menuItem);
+            window.document.getElementById('appmenu_webDeveloper_popup').removeChild(appmenu_resteasy);
+            window.document.getElementById('menuWebDeveloperPopup').removeChild(menu_resteasy);
         });
     });
 }
