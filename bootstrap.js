7929ae99775e30f9b533f3cce48cbcbf2166cafe
Nathan Osman <admin@quickmediasolutions.com>
2013-11-15 19:08:45 -0800

    Added support for Firefox Mobile for Android.

--- bootstrap.js
+++ bootstrap.js
@@ -9,7 +9,7 @@ function startup(data, reason) {
     watchWindows(function(window) {
         
         // Creates and returns the menu item with the provided prefix in the provided menu
-        function createMenuItem(menu, prefix) {
+        function createDesktopMenuItem(menu, prefix) {
             
             var menuItem = window.document.createElementNS(NS_XUL, 'menuitem');
             menuItem.setAttribute('id',        prefix + '_resteasy');
@@ -18,8 +18,7 @@ function startup(data, reason) {
             menuItem.addEventListener('command', function() {
                 
                 // Open the main.html page in a new tab
-                var tab = window.document.getElementById('content').addTab('chrome://resteasy/content/main.html');
-                tab.parentElement.selectedItem = tab;
+                window.gBrowser.selectedTab = window.gBrowser.addTab('chrome://resteasy/content/main.html');
                 
             }, true);
             
@@ -29,16 +28,36 @@ function startup(data, reason) {
             return menuItem;
         }
         
-        // Add it to the toolbar
-        var appmenu_resteasy = createMenuItem('appmenu_webDeveloper_popup', 'appmenu');
-        var menu_resteasy    = createMenuItem('menuWebDeveloperPopup',      'menu');
+        // Check if we are mobile
+        var mobile = typeof window.NativeWindow != 'undefined';
         
-        // Remember to remove the menu item when the addon is unloaded
-        unload(function() {
+        if(mobile) {
             
-            window.document.getElementById('appmenu_webDeveloper_popup').removeChild(appmenu_resteasy);
-            window.document.getElementById('menuWebDeveloperPopup').removeChild(menu_resteasy);
-        });
+            // Add the item to the native menu
+            var menu_resteasy = window.NativeWindow.menu.add("REST Easy", null, function() {
+                
+                window.BrowserApp.addTab('chrome://resteasy/content/main.html')
+            });
+            
+            // Remove the item when the addon is unloaded
+            unload(function() {
+                
+                window.NativeWindow.menu.remove(menu_resteasy);
+            });
+            
+        } else {
+            
+            // Add it to the appropriate menus
+            var appmenu_resteasy = createDesktopMenuItem('appmenu_webDeveloper_popup', 'appmenu');
+            var menu_resteasy    = createDesktopMenuItem('menuWebDeveloperPopup',      'menu');
+            
+            // Remove the menu items when the addon is unloaded
+            unload(function() {
+                
+                window.document.getElementById('appmenu_webDeveloper_popup').removeChild(appmenu_resteasy);
+                window.document.getElementById('menuWebDeveloperPopup').removeChild(menu_resteasy);
+            });
+        }
     });
 }
 
