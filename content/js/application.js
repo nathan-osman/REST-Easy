11fd70ebeb3edd931ad9d2de37c2a9df4931d9bc
Nathan Osman <nathan@quickmediasolutions.com>
2015-04-14 23:51:05 -0700

    Implemented double-click to edit values in tables (fixes #32).

--- content/js/application.js
+++ content/js/application.js
@@ -311,6 +311,7 @@ RESTEasy.EditableTableComponent = Ember.Component.extend({
     trName: tr('table.name'),
     trValue: tr('table.value'),
     trRemove: tr('table.remove'),
+    trSave: tr('table.save'),
     trAdd: tr('table.add'),
 
     actions: {
@@ -319,13 +320,22 @@ RESTEasy.EditableTableComponent = Ember.Component.extend({
                 value = this.get('value');
             // Don't add anything unless both fields are filled in
             if(name && value) {
-                this.get('entries').pushObject({
-                    name: name,
-                    value: value
-                });
+                this.get('entries').pushObject(
+                    Ember.Object.create({
+                        editing: false,
+                        name: name,
+                        value: value
+                    })
+                );
                 this.set('name', '');
                 this.set('value', '');
             }
+        },
+        edit: function(o) {
+            o.set('editing', true);
+        },
+        save: function(o) {
+            o.set('editing', false);
         },
         remove: function(o) {
             this.get('entries').removeObject(o);
